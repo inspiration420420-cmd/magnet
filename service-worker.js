0aaaa94749e3a3d2cd916ad765bc361c8eecbf1d
inspiration420420-cmd <inspiration420420@gmail.com>
2026-09-16 17:41:45 +0200

    Bump offline cache for challenge update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='magnet-mayhem-v4';
+const CACHE='magnet-mayhem-v5';
 const ASSETS=['./','./index.html','./style.css','./game2.js','./systems.js','./mobile-enhancements.js','./boss-system.js','./challenge-system.js','./accessibility-controls.js','./gameplay-overdrive.js','./polish-system.js','./level-select.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
