ee1e52be9a56d1d48c26fcc076bd9d84b638af6c
inspiration420420-cmd <inspiration420420@gmail.com>
2026-09-16 17:27:27 +0200

    Cache level selector and refresh PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='magnet-mayhem-v2';
-const ASSETS=['./','./index.html','./style.css','./game2.js','./systems.js','./mobile-enhancements.js','./boss-system.js','./challenge-system.js','./accessibility-controls.js','./gameplay-overdrive.js','./polish-system.js','./manifest.json','./icon.svg'];
+const CACHE='magnet-mayhem-v3';
+const ASSETS=['./','./index.html','./style.css','./game2.js','./systems.js','./mobile-enhancements.js','./boss-system.js','./challenge-system.js','./accessibility-controls.js','./gameplay-overdrive.js','./polish-system.js','./level-select.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
